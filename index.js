c74a92ed315fcf3dfaae413d35607f7bc7bbd116
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-25 14:11:45 +0300

    data created for courses

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/courses.json')
+
 app.use(cors())
+
+
 
 app.get('/', (req, res) => {
     res.send('hello world')
